8c7eb60b7505fd00c764952e838dff669b495db6
24aeronyx <11211017@student.itk.ac.id>
2024-11-20 02:31:06 +0800

    create: get monthly report route and controller

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,10 @@ const route = express.Router()
 const authRoute = require('../routes/auth-route')
 const profileRoute = require('../routes/profile-route')
 const transactionRoute = require('../routes/transaction-route')
+const { authenticateJWT } = require("../middleware/auth")
 
 route.use('/auth', authRoute)
 route.use('/profile', profileRoute)
-route.use('/transaction', transactionRoute)
+route.use('/transaction', authenticateJWT, transactionRoute)
 
 module.exports = route
--- routes/transaction-route.js
+++ routes/transaction-route.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const { addIncome, addOutcome } = require("../controller/transaction-controller");
-const { authenticateJWT } = require("../middleware/auth");
+const { addIncome, addOutcome, getMonthlyReport } = require("../controller/transaction-controller");
 const router = express.Router();
 
-router.post('/add-income', authenticateJWT, addIncome)
-router.post('/add-outcome', authenticateJWT, addOutcome)
+router.post('/add-income',  addIncome)
+router.post('/add-outcome',  addOutcome)
+router.get('/monthly-report',  getMonthlyReport)
 
 module.exports = router
