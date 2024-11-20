1c6c01171f89eae248a7593f51b706fd9970b21d
24aeronyx <11211017@student.itk.ac.id>
2024-11-20 14:00:16 +0800

    create: get profile controller and routes

--- controller/profile-controller.js
+++ controller/profile-controller.js
@@ -22,6 +22,28 @@ const profileController = {
             console.error('Error fetching balance:', error)
             res.status(500).json({message:'Internal server error'})
         }
+    },
+
+    getProfile: async(req, res) =>{
+        try{
+            const{credential_id} = req.user
+
+            const profile = await Profile.findOne({
+                where: {credential_id}
+            })
+
+            if(!profile){
+                return res.status(404).json({message: 'Profile not found'})
+            }
+
+            res.status(200).json({
+                message: "Profile fetched successfully",
+                profile
+            })
+        } catch(error){
+            console.error('Error fetching profile', error)
+            return res.status(500).json({message: 'Internal server error'})
+        }
     }
 }
 
--- routes/profile-route.js
+++ routes/profile-route.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { getBalance } = require("../controller/profile-controller");
+const { getBalance, getProfile } = require("../controller/profile-controller");
 const { authenticateJWT } = require("../middleware/auth");
 const router = express.Router();
 
 router.get('/balance', authenticateJWT, getBalance)
+router.get('/', authenticateJWT, getProfile)
 
 module.exports = router
