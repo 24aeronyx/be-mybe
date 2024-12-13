bbfa94f5bc2db62f569cb655cb7a3082d43f3c79
24aeronyx <11211017@student.itk.ac.id>
2024-12-13 22:03:24 +0800

    Update cors

--- app.js
+++ app.js
@@ -7,7 +7,13 @@ require('dotenv').config();
 const PORT = process.env.PORT
 
 app.use(express.json())
-app.use(cors());
+app.use(
+    cors({
+      origin: "*",
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+    })
+  );
 app.use(allRoutes)
 
 app.listen(PORT, ()=>{
